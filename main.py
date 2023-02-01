59a4592c1abb4359a5969340c22ef9d953603267
Zelin Chen <6692czl@gmail.com>
2023-01-31 17:31:17 -0800

    3rd

--- main.py
+++ main.py
@@ -2,15 +2,13 @@ def main():
    ##################################################
    # Comlete your code here
    ##################################################
+    RP = int(input('Regular Price: '))
+    RT = int(input('Rate: '))
+    print('Regular Price:  $', format(RP, '.0f'))
+    print('Discount Amout: $', format(RP*RT/100, '.0f'))
+    print('The final price:$', format(RP-RP*RT/100, '.0f'))
 
     pass
-
-
-RP = int(input('Regular Price: '))
-RT = int(input('Rate: '))
-print('Regular Price:  $', format(RP, '.0f'))
-print('Discount Amout: $', format(RP*RT/100, '.0f'))
-print('The final price:$', format(RP-RP*RT/100, '.0f'))
 
 
 if __name__ == '__main__':
